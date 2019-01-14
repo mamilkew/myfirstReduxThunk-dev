79171e6a5f235f930027db6a554cf166e8563b63
mamilkew <mui_milk@hotmail.com>
2019-01-14 11:48:20 +0700

    Stateful React component: Refactoring to work with Redux store methods

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./js/components/List";
+import Form from "./js/components/Form"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// const App = () => (
+//     <List />
+// );
+
+const App = () => (
+    <div className="row mt-5">
+        <div className="col-md-4 offset-md-1">
+            <h2>Articles</h2>
+            <List/>
+        </div>
+        <div className="col-md-4 offset-md-1">
+            <h2>Add a new article</h2>
+            <Form/>
+        </div>
+    </div>
+);
 
 export default App;
+
+
+// ======================== Initial App ===============================
+
+// import React, {Component} from 'react';
+// // import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             articles: [
+//                 {title: "React Redux Tutorial for Beginners", id: 1},
+//                 {title: "Redux e React: cos'è Redux e come usarlo con React", id: 2}
+//             ]
+//         };
+//     }
+//
+//     render() {
+//         const {articles} = this.state;
+//         return <ul>{articles.map(el => <li key={el.id}>{el.title}</li>)}</ul>;
+//     }
+// }
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import index from "./js/index"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// ======================== Case 2 ===============================
+import {Provider} from "react-redux";
+import store from "./js/store/index";
+
+const MyApp = () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
+
+ReactDOM.render(<MyApp/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
